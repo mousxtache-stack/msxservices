07370626b83c0299d3cb6a9479bbf54b8e1102fd
Clement <clement10600@gmail.com>
2025-02-13 18:51:36 +0100

    Résolution du conflit et mise à jour de la config du serveur

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,18 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 import fs from "fs";
 
-
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-<<<<<<< HEAD
-    server: {
-      https: {
-        key: fs.readFileSync("localhost-key.pem"), // ton fichier clé privée
-        cert: fs.readFileSync("localhost-cert.pem"), // ton fichier certificat
-      },
-    },
-=======
   server: {
+    // Si tu veux utiliser HTTPS, garde cette partie, sinon, tu peux la supprimer
+    https: {
+      key: fs.readFileSync("localhost-key.pem"), // ton fichier clé privée
+      cert: fs.readFileSync("localhost-cert.pem"), // ton fichier certificat
+    },
+    // Ou si tu veux plutôt utiliser cette configuration pour HTTP
     host: "::",
     port: 8080,
     allowedHosts: [
@@ -26,7 +21,6 @@ export default defineConfig(({ mode }) => ({
       "*.lovable.app"
     ]
   },
->>>>>>> 2f797ec6778708bca0b2fdc2364c1818e8a3ace1
   plugins: [
     react(),
     mode === 'development' &&
